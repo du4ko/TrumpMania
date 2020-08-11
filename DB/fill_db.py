42fd0469ab7e3f614edd3597b4b71f9438396ac1
Viktor <viktorstanchev7@gmail.com>
2020-08-11 21:38:41 +0300

    Added some comments for clarification and a collection check to not insert if the json is already pushed

--- DB/fill_db.py
+++ DB/fill_db.py
@@ -6,15 +6,20 @@ import sys
 
 
 def main_db_operation():
-    #to check if the db is already created and filled.
-    
+
     #get the json from the url
     json = dl_json_("http://trumptwitterarchive.com/data/realdonaldtrump/2018.json")
 
+    #initiate a client with the username and password for the free cluster provided by Atlas
     client = MongoClient("mongodb+srv://duchko:TryFart09@cluster0.2ylep.mongodb.net/DonalDB?retryWrites=true&w=majority")
+
     #insert the data
     db = client["DonalDB"]
     collection = db["DonalDB"]
-    result = collection.insert_many(json)
 
+    #check if the data is not already pushed
+    if(collection.count() == 0):
+        result = collection.insert_many(json)
+    
+    #close the connection
     client.close()
